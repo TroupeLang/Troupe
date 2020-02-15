a18c59e2dbfb24676e9abfde3b9f187f8f40341c
Aslan Askarov <aslan@askarov.net>
2020-02-15 23:53:25 +0100

    Adding trustmap generation option to makealiases

--- rt/src/p2p/mkaliases.js
+++ rt/src/p2p/mkaliases.js
@@ -19,14 +19,34 @@ const files=argv['include'];
 const outfile=argv['outfile']
 
 let aliases = {}
+let trustmap = []
+
 
 for (let i in files) {
   let fname = files[i]
   let json = JSON.parse (fs.readFileSync(fname));
   let alias = path.basename(fname, ".json")
   aliases[alias] = json.id   
+  if (argv.trustmap) {
+    let level; 
+    
+    if (argv.droptrustprefix) {
+      let j = alias.indexOf(argv.droptrustprefix) ;
+      console.log (j, argv.droptrustprefix)
+      if (j == 0) {
+        level = alias.substr (argv.droptrustprefix.length)
+      } else {
+        level = alias;
+      }
+      console.log (level)
+    }
+    trustmap.push ( {id:json.id, level:level})
+
+  }
 }
 
 fs.writeFileSync (outfile, JSON.stringify(aliases), 'utf8');
-
+if (argv.trustmap) {
+  fs.writeFileSync (argv.trustmap, JSON.stringify (trustmap), 'utf8')
+}
 

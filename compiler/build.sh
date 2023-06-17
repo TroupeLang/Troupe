41f60faee3aaca2c9574a0488f5b96f8427b083a
Felix Wiemuth <533601+felixwiemuth@users.noreply.github.com>
2023-06-17 14:34:12 +0200

    Document and move utility build script

--- compiler/build.sh
+++
@@ -1,7 +0,0 @@
-# Builds and copies the executables to ../bin/<current git HEAD hash>
-make
-echo ""
-dir="./../bin/$(git describe --long --dirty --always)"
-mkdir -p $dir
-cp ./../bin/* $dir 2>/dev/null
-echo "Copied executables to $dir"
